31dc860c20e901130bfd4b26243ae9abdf3ca623
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-03-06 10:23:04 -0500

    added api and html routing to project

---
+++ server.js
@@ -0,0 +1,23 @@
+// start express and assign to app variable so that we can use the methods later
+const express = require('express');
+const app = express();
+// assign the PORT from heroku
+const PORT = process.env.PORT || 3001;
+// include node modules to be used
+const fs = require('fs');
+const path = require('path');
+// include files for server file to use
+const { notes } = require('./data/notes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// instantiate the server
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
